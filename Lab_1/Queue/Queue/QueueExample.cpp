450e7f1d0a99e1bc8740a901abba4d74517f9dc1
NiteWatch <fdesaleg@stumail.jccc.edu>
2017-01-25 17:08:28 -0600

    Wrapping up Lab 1 Exercises

--- Lab_1/Queue/Queue/QueueExample.cpp
+++ Lab_1/Queue/Queue/QueueExample.cpp
@@ -6,13 +6,27 @@ using namespace std;
 int main()
 {
 	float balance = 0.0;
+
 	// Create a queue of floats, named transactions
+	queue<float> transactions;
 
 	// Push some + and - floats into the transactions queue.
+	transactions.push(200.50);
+	transactions.push(152);
+	transactions.push(20);
+	transactions.push(68.23);
+	transactions.push(90.50);
 
 	// Loop through all the transactions - while the queue is empty.
 	// Within the loop, whatever amount is at the front of the queue,
 	// add it to the balance and then pop it off the queue.
+	while (!transactions.empty())
+	{
+		float amount = transactions.front();
+		cout << amount << " is pushed to the account" << endl;
+		balance += amount;
+		transactions.pop();
+	}
 
 	cout << "Final balance: $" << balance << endl;
 
