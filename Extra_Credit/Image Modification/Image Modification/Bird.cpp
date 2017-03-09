d02eda1f1b342dad640a35e7b0265d48b0cb616f
NiteWatch <svc-student@student.directory.jccc>
2017-03-08 20:40:29 -0600

    Working on Extra_Credit

--- Extra_Credit/Image Modification/Image Modification/Bird.cpp
+++ Extra_Credit/Image Modification/Image Modification/Bird.cpp
@@ -5,5 +5,17 @@
 
 int main()
 {
+	ifstream input("bird.ppm");
+	string buffer;
+	string header;
 
+	// Save the header:
+	getline(input, buffer); // ascii code
+	header += buffer + "\n";
+	getline(input, buffer); // comment
+	header += buffer + "\n";
+	getline(input, buffer); // width/height
+	header += buffer + "\n";
+	getline(input, buffer); // max color
+	header += buffer + "\n";
 }
